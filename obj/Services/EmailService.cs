9af61f583a75bc265d3acb42d1efc88e07a9a25a
andre31072003 <andre31072003@gmail.com>
2026-01-14 22:00:33 +0000

    Add files via upload

---
+++ obj/Services/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Mail;
+using System.Net;
+using TrabalhoLab.Models;
+
+namespace TrabalhoLab.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            using var client = new SmtpClient(_emailSettings.MailServer, _emailSettings.MailPort)
+            {
+                Credentials = new NetworkCredential(_emailSettings.Sender, _emailSettings.Password),
+                EnableSsl = true,
+            };
+
+            await client.SendMailAsync(new MailMessage
+            {
+                From = new MailAddress(_emailSettings.Sender, _emailSettings.SenderName),
+                To = { new MailAddress(email) },
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = true
+            });
+        }
+    }
+}
